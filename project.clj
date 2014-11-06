d543908c850d7415c9a19733a585be98ae209024
tangrammer <juanantonioruz@gmail.com>
2014-11-06 10:09:32 +0100

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tangrammer/wrapper "0.1.1-SNAPSHOT"
+(defproject tangrammer/wrapper "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
